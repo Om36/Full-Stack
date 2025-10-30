be4ab9a800c1d75c71eb1d618541560c3991df99
Om36 <masterom36@gmail.com>
2025-10-31 01:14:08 +0530

    Update files

---
+++ Exp9.1/my-react-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Hello, ByteXL!</h1>
+      <p>Practice 1 - Cloud Deployment: Single File React App.</p>
+    </div>
+  );
+}
+export default App;
